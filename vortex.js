2661c53fdc6f310e3223b33fa68de56f94cff1ff
VorteX <63967719+SeonerVorteX@users.noreply.github.com>
2022-06-13 16:33:38 +0400

    Update vortex.js

--- vortex.js
+++ vortex.js
@@ -15,16 +15,16 @@ require('./src/handlers/eventHandler.js');
 
 // Checking Commands
 readdirSync('./src/commands').filter(async dir => {
-  const commandFiles = readdirSync(`./src/commands/${dir}/`).filter(file => file.endsWith('.js'));
-  for (const file of commandFiles) {
-    const command = require(`./src/commands/${dir}/${file}`);
-    await client.commands.set(command.name, command);
-    // console.log(`[COMMAND] ${command.name} Loaded!`);
+    const commandFiles = readdirSync(`./src/commands/${dir}/`).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const command = require(`./src/commands/${dir}/${file}`);
+        await client.commands.set(command.name, command);
+        // console.log(`[COMMAND] ${command.name} Loaded!`);
   }
 });
 
 // Connecting To Client
 client.login(Token).then(() => console.log('[BOT] Connection started')).catch(() => {
-  console.log('[BOT] Failed to start connection, trying again');
-  process.exit();
+    console.log('[BOT] Failed to start connection, trying again');
+    process.exit();
 });
